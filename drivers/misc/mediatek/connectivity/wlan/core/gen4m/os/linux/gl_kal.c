c15db1c04dd44581681abbbebe21e829e91d76b9
bengris32 <bengris32@protonmail.ch>
2024-01-24 17:49:12 +0000

    drivers: connectivity: gen4m: Silence more debug logging
    Change-Id: Ic176c9b20b909b233bf07eb613fb04f842fe2e38
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/gl_kal.c
+++ drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/gl_kal.c
@@ -9895,6 +9895,7 @@ void kalUpdateCompHdlrRec(IN struct ADAPTER *prAdapter,
 					% OID_HDLR_REC_NUM;
 }
 
+#if (BUILD_QA_DBG == 1)
 void kalPrintUTC(char *msg_buf, int msg_buf_size)
 {
 	int ret = 0;
@@ -9977,7 +9978,7 @@ void kalPrintLog(const char *fmt, ...)
 
 	va_end(args);
 }
-
+#endif
 
 #if (CFG_SUPPORT_POWER_THROTTLING == 1)
 void kalPwrLevelHdlrRegister(IN struct ADAPTER *prAdapter,
