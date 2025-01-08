ac00e024f9ac1ec9eb6f3392acf217f331dc8e34
Bardia Moshiri <bardia@furilabs.com>
2025-01-07 20:20:44 -0500

    debian: drop buildvariant from cmdline
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -11,7 +11,7 @@ VARIANT = android
 KERNEL_BASE_VERSION = 4.19.325
 
 # The kernel cmdline to use
-KERNEL_BOOTIMAGE_CMDLINE = bootopt=64S3,32N2,64N2 buildvariant=userdebug
+KERNEL_BOOTIMAGE_CMDLINE = bootopt=64S3,32N2,64N2
 
 # Slug for the device vendor. This is going to be used in the KERNELRELASE
 # and package names.
